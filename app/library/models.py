ab379efe9174820a4623e1956a069c46d3d1dc6a
Thuc <dpham@datagene.com.au>
2020-08-22 20:20:14 +1000

    Added model for the library app

--- app/library/models.py
+++ app/library/models.py
@@ -1,5 +1,55 @@
 from django.db import models
 
-# Create your models here.
+# class Tag(models.Model):
+#     """Tag to be used for a book"""
+#     name = models.CharField(max_length=255)
+    
+#     def __str__(self):
+#         return self.name
+
+
+class Library(models.Model):
+    """Library object"""
+    name = models.CharField(max_length=255, default='')
+    address = models.CharField(max_length=255, default='')
+
+    def __str__(self):
+        return self.name
+
+
+class Author(models.Model):
+    """Author object"""
+    name = models.CharField(max_length=255, default='')
+
+    def __str__(self):
+        return self.name
+
+
 class Book(models.Model):
     """Book object"""
+    title = models.CharField(max_length=255)
+    library = models.ForeignKey(
+        Library,
+        on_delete=models.CASCADE,
+        related_name='books',
+    )
+    # tags = models.ManyToManyField('Tag')
+
+    def __str__(self):
+        return self.name
+
+    def get_page_count(self):
+        return self.pages.count()
+
+
+class Page(models.Model):
+    """Page object"""
+    book = models.ForeignKey(
+        Book,
+        on_delete=models.CASCADE,
+        related_name='pages',
+    )
+    # Unlike CharField, TextField does not limit 255 max length
+    text = models.TextField(null=True, blank=True)
+    page_number = models.IntegerField()
+
